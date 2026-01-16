a720a356bef421cc41ff9bb6c7981ab5235e1e0d
eduardo-leitner <eduardo.leitner@tesouro.gov.br>
2026-01-16 16:55:37 -0300

    Adicionei a pasta da sessão 6

---
+++ FISLAC/Sessão 6 - Modelo RBC de Economia Aberta/RBC_abierto/model/julia/SparseStaticG1TT!.jl
@@ -0,0 +1,7 @@
+function SparseStaticG1TT!(T::Vector{<: Real}, y::Vector{<: Real}, x::Vector{<: Real}, params::Vector{<: Real})
+    SparseStaticResidTT!(T, y, x, params)
+@inbounds begin
+end
+    return nothing
+end
+
